8b9f2861551df74271a7df3ab3f5fce1ccd1cb3d
Austinspeed <austinikedinma@gmail.com>
2026-03-03 07:14:38 +0100

    Added button to MealDetailScreen header

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,13 +1,30 @@
+import { useLayoutEffect } from "react";
 import { Image, Text, View, StyleSheet, ScrollView } from "react-native";
 
 import { MEALS } from "../data/dummy-data";
 import MealDetails from "../components/MealDetails";
 import Subtitle from "../components/MealDetail/Subtitle";
 import List from "../components/MealDetail/List";
+import IconButton from "../components/IconButton";
 
-const MealDetailScreen = ({ route }) => {
+const MealDetailScreen = ({ route, navigation }) => {
   const mealId = route.params.mealId;
   const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+
+  function headerButtonPressHandler() {
+    console.log("Pressed!");
+  }
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => {
+        return (
+          <IconButton icon='star' color='white' onPress={headerButtonPressHandler} />
+        );
+      },
+    });
+  }, [navigation, headerButtonPressHandler]);
+
   return (
     <ScrollView style={styles.rootContainer}>
       <Image style={styles.image} source={{ uri: selectedMeal.imageUrl }} />
@@ -20,13 +37,13 @@ const MealDetailScreen = ({ route }) => {
       />
 
       <View style={styles.listOuterContainer}>
-              <View style={styles.listContainer}>
-        <Subtitle>Ingredients</Subtitle>
-        <List data={selectedMeal.ingredients} />
+        <View style={styles.listContainer}>
+          <Subtitle>Ingredients</Subtitle>
+          <List data={selectedMeal.ingredients} />
 
-        <Subtitle>Steps</Subtitle>
-        <List data={selectedMeal.steps} />
-      </View>
+          <Subtitle>Steps</Subtitle>
+          <List data={selectedMeal.steps} />
+        </View>
       </View>
     </ScrollView>
   );
@@ -35,9 +52,9 @@ const MealDetailScreen = ({ route }) => {
 export default MealDetailScreen;
 
 const styles = StyleSheet.create({
-    rootContainer: {
-        marginBottom: 32
-    },
+  rootContainer: {
+    marginBottom: 32,
+  },
   image: {
     width: "100%",
     height: 350,
@@ -52,8 +69,8 @@ const styles = StyleSheet.create({
   detailText: {
     color: "white",
   },
-    listOuterContainer: {
-    alignItems: 'center'
+  listOuterContainer: {
+    alignItems: "center",
   },
   listContainer: {
     width: "80%",
